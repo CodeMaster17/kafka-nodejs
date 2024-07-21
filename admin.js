c419620f58033afdc5a836ddf4c042fd79d9016f
NaturesClub <films.saswat@gmail.com>
2024-07-21 20:57:36 +0530

    feat: added comments in admin.js Added readme

--- admin.js
+++ admin.js
@@ -1,10 +1,17 @@
+// admin will create and manage topics
+
 const { kafka } = require('./client');
 async function init() {
+
+
+    // -------- connecting admin --------------
     const admin = kafka.admin();
     console.log('Connecting admin...');
     admin.connect();
     console.log('admin connecting success...');
 
+    // ------ topics -----------
+    console.log('Creating topic [rider-updates]')
     await admin.createTopics({
         topics: [
             {
@@ -14,6 +21,7 @@ async function init() {
         ]
     });
     console.log('Topic created successfully [rider-updates]')
+    
     console.log('Disconnecting admin...')
     // disconnect the admin
     await admin.disconnect();
